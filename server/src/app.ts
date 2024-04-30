ffc28f5054d6eef18b26348ab6d359b169ebc7a6
vivek <chimnanivivek14@gmail.com>
2024-04-30 14:34:27 +0530

    first api route

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,9 +1,17 @@
 import express from "express";
 import mongoose from "mongoose";
+import cors from "cors";
+import userRoutes from "./routes/user";
 import dotenv from "dotenv";
+
 dotenv.config();
 
 const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/user", userRoutes);
 
 app.listen(3000, () => {
   console.log("Server is listening!");
---
+++ server/src/controllers/user.ts
@@ -0,0 +1,6 @@
+import { RequestHandler } from "express";
+
+export const register: RequestHandler = async (req, res) => {
+  try {
+  } catch (error) {}
+};
