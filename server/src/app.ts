a80075fa36896800b08d84694255a82f46a63394
vivek <chimnanivivek14@gmail.com>
2024-04-30 14:26:08 +0530

    connected to database

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,7 +1,17 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
 
 const app = express();
 
 app.listen(3000, () => {
   console.log("Server is listening!");
 });
+
+mongoose
+  .connect(process.env.MONGO_URL!)
+  .then(() => {
+    console.log("Database connected!");
+  })
+  .catch((err) => console.log(`Error: ${err}`));
