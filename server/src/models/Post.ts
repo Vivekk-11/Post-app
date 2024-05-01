ce74233d4e20763573d68b0fa3b9dbeeba50c90e
vivek <chimnanivivek14@gmail.com>
2024-05-01 11:42:18 +0530

    Post model

---
+++ server/src/models/Post.ts
@@ -0,0 +1,34 @@
+import mongoose, { Document, Model, ObjectId } from "mongoose";
+const { Schema } = mongoose;
+
+export interface IPost extends Document {
+  title: string;
+  description: string;
+  image: string;
+  creator: ObjectId;
+}
+
+const postSchema = new Schema(
+  {
+    title: { required: true, type: String, minLength: 3 },
+    description: { required: true, type: String, minLength: 10 },
+    image: {
+      type: String,
+      required: true,
+    },
+    creator: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+type PostModel = Model<IPost>;
+
+const Post: PostModel =
+  mongoose.models.Post || mongoose.model<IPost>("Post", postSchema);
+
+export default Post;
